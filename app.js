83aa26d93bd99e1f3eb24b19dc89fe9c8c11697e
eldilibra <writeme@ldlibra.com>
2013-03-23 18:25:09 -0400

    more structure, added home

--- app.js
+++ app.js
@@ -4,19 +4,17 @@
  */
 
 var express = require('express')
-  , lib = require('./lib')
   , http = require('http')
   , path = require('path');
 
 var app = express();
-var landing = require('./lib/landingPage');
+var landing = require('./lib/landing');
+var home = require('./lib/home');
 var users = require('./lib/users');
 var pieces = require('./lib/pieces');
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -25,6 +23,7 @@ app.configure(function(){
   app.use(express.session());
   app.use(app.router);
   app.use(landing);
+  app.use(home);
   app.use(users);
   app.use(pieces);
   app.use(express.static(path.join(__dirname, 'public')));
