3775843a2034ec8b10326490c149cd8f13f9256b
eldilibra <writeme@ldlibra.com>
2013-05-11 20:47:10 -0400

    form submission

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -12,3 +12,10 @@ inputArr.forEach(function inputForEach (input) {
     }
   });
 });
+
+var submit = document.getElementsByClassName('submit')[0];
+var form = document.getElementsByTagName('form')[0];
+
+submit.addEventListener('click', function submitForm () {
+  document.forms[form.name].submit();
+});
